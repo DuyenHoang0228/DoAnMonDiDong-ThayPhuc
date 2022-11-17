0f49e0f6565891576f91d5e94b1a2977b2319253
DuyenHoang0228 <hoangduyen02281@gmail.com>
2022-11-17 17:57:14 +0700

    trang chủ, chưa có adaptersanpham

--- app/src/main/java/com/nhom4/lilpawhome_application/MainActivity.java
+++ app/src/main/java/com/nhom4/lilpawhome_application/MainActivity.java
@@ -1,15 +1,59 @@
 package com.nhom4.lilpawhome_application;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+import com.nhom4.adapters.Danhmuc1Adapter;
+import com.nhom4.lilpawhome_application.databinding.ActivityMainBinding;
+import com.nhom4.models.DanhMuc1;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityMainBinding binding;
+    Danhmuc1Adapter adapter;
+    ArrayList<DanhMuc1> danhmuc;
+    private ImageSlider imageSlider;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+       // setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        // setContentView(binding.getRoot());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        imageSlider = findViewById(R.id.image_slider);
+        ArrayList<SlideModel> slideModels = new ArrayList<>();
+        slideModels.add(new SlideModel(R.drawable.icon_shopchocho_home,ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.icon_shopchocho_home,ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.icon_shopchocho_home,ScaleTypes.FIT));
+
+         imageSlider.setImageList(slideModels,ScaleTypes.FIT);
+        
+
+        loadData();
+    }
+
+
+    private void loadData() {
+        danhmuc = new ArrayList<>();
+        danhmuc.add(new DanhMuc1(R.drawable.icon_shopchocho_home,"Shop cho Chó"));
+        danhmuc.add(new DanhMuc1(R.drawable.icon_shopchomeo_home,"Shop cho Mèo"));
+        danhmuc.add(new DanhMuc1(R.drawable.icon_spa_home,"Spa"));
+        danhmuc.add(new DanhMuc1(R.drawable.icon_thuonghieu_home,"Thương Hiệu"));
+        danhmuc.add(new DanhMuc1(R.drawable.icon_uudai_home,"Ưu đãi"));
+        danhmuc.add(new DanhMuc1(R.drawable.icon_blog_home,"Blog"));
+
+        adapter = new Danhmuc1Adapter(MainActivity.this,R.layout.danhmuc1_list,danhmuc);
+        binding.gvDanhmuchome.setAdapter(adapter);
+
+
     }
 }
