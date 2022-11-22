b636f4cc6fdb6eae22b897df2d800c027a1f5371
DuyenHoang0228 <108680922+DuyenHoang0228@users.noreply.github.com>
2022-11-23 00:40:42 +0700

    Add files via upload

--- app/src/main/java/com/nhom4/lilpawhome_application/MainActivity.java
+++ app/src/main/java/com/nhom4/lilpawhome_application/MainActivity.java
@@ -2,13 +2,7 @@ package com.nhom4.lilpawhome_application;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 }
