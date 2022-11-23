67a89805156e61f0434ec0663e67f8182def55aa
kimmhanhh <ptrkimhanh@gmail.com>
2022-11-23 13:21:22 +0700

    23/11/2022

---
+++ app/src/main/java/com/nhom4/models/BannerUuDai.java
@@ -0,0 +1,17 @@
+package com.nhom4.models;
+
+public class BannerUuDai {
+    String imgUrl;
+
+    public BannerUuDai(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+}
