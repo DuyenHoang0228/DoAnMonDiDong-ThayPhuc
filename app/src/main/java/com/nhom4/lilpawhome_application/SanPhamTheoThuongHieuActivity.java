87b942f9f5d9d6e553b1b820db0fa3d0977b8023
nguyenngoctham02da <thamnn20411@st.uel.edu.vn>
2022-11-23 08:06:20 +0700

    Đã hoàn thành: Thương Hiệu (activity và layout), Sản phẩm theo thương hiệu (layout), Shop cho chó 1 (layout và dữ liệu đổ bằng cơm), Trang sản phẩm (layout, chưa intent được dữ liệu)

---
+++ app/src/main/java/com/nhom4/lilpawhome_application/SanPhamTheoThuongHieuActivity.java
@@ -0,0 +1,49 @@
+package com.nhom4.lilpawhome_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nhom4.adapters.SanphamAdapter;
+import com.nhom4.lilpawhome_application.databinding.ActivitySanPhamTheoThuongHieuBinding;
+import com.nhom4.models.SanPham;
+
+import java.util.ArrayList;
+
+public class SanPhamTheoThuongHieuActivity extends AppCompatActivity {
+    ActivitySanPhamTheoThuongHieuBinding binding;
+    SanphamAdapter adapter;
+    ArrayList<SanPham> sanPhamArrayList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_san_pham_theo_thuong_hieu);
+        binding=ActivitySanPhamTheoThuongHieuBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        loadData();
+    }
+
+    private void loadData() {
+        sanPhamArrayList=new ArrayList<>();
+        sanPhamArrayList.add(new SanPham(R.drawable.sphatcho,"Hạt cho chó",120000,200000,
+                "Thương hiệu 1","thucanchocho","hatchocho"));
+        sanPhamArrayList.add(new SanPham(R.drawable.sppatecho,"Pate cho chó",350000,400000,
+                "Thương hiệu 1","thucanchocho","patechocho"));
+        sanPhamArrayList.add(new SanPham(R.drawable.spsuacho,"Sữa tắm chó",250000,300000,
+                "Thương hiệu 2","thucanchocho","suacho"));
+        sanPhamArrayList.add(new SanPham(R.drawable.spsuatamcho,"Sữa tắm chó",120000,320000,
+                "Thương hiệu 2","dodungcho","suatamcho"));
+        sanPhamArrayList.add(new SanPham(R.drawable.spxuongcho,"Xương chó đồ chơi",20000,50000,
+                "Thương hiệu 3","dochoicho","xuongcho"));
+        sanPhamArrayList.add(new SanPham(R.drawable.spdinhduongcho,"Sữa dinh dưỡng cho chó",360000,500000,
+                "Thương hiệu 3","thucanchocho","dinhduongchocho"));
+        sanPhamArrayList.add(new SanPham(R.drawable.sptaimatmiengcho,"Cây chà răng chó",25000,40000,
+                "Thương hiệu 4","dodungcho","taimatcho"));
+
+        adapter=new SanphamAdapter(SanPhamTheoThuongHieuActivity.this,R.layout.sanpham_list,sanPhamArrayList);
+        binding.gvSanphamtheothuonghieu.setAdapter(adapter);
+
+    }
+}
