83e22bf693bc7ad4c4f92dbb9756dd36b1cb3b41
XuanLeThiHong <xuanlth20411@st.uel.edu.vn>
2022-11-24 20:33:10 +0700

    sơ sơ hoi

---
+++ app/src/main/java/com/nhom4/lilpawhome_application/themtaikhoannganhang.java
@@ -0,0 +1,14 @@
+package com.nhom4.lilpawhome_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class themtaikhoannganhang extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_themtaikhoannganhang);
+    }
+}
