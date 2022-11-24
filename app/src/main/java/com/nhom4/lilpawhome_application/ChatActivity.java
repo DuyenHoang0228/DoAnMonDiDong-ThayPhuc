ff28681b48cf423a0ce63decc28b6f62ab3c896a
DuyenHoang0228 <hoangduyen02281@gmail.com>
2022-11-24 23:21:43 +0700

    đổ dữ liệu help vào string

--- app/src/main/java/com/nhom4/lilpawhome_application/ChatActivity.java
+++ app/src/main/java/com/nhom4/lilpawhome_application/ChatActivity.java
@@ -2,26 +2,16 @@ package com.nhom4.lilpawhome_application;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 import com.nhom4.adapters.ChatAdapter;
-import com.nhom4.adapters.MessageAdapter;
 import com.nhom4.lilpawhome_application.databinding.ActivityChatBinding;
-import com.nhom4.lilpawhome_application.databinding.ActivityHelpBinding;
 import com.nhom4.models.message;
 
 import java.util.ArrayList;
-import java.util.List;
-
-import kotlin.contracts.Returns;
 
 public class ChatActivity extends AppCompatActivity {
     ActivityChatBinding binding;
