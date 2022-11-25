3ed976d5bed38b7625d86351d718ed09cafc9315
DuyenHoang0228 <hoangduyen02281@gmail.com>
2022-11-25 19:01:00 +0700

    Câp nhật trang chủ

--- app/src/main/java/com/nhom4/lilpawhome_application/MainActivity.java
+++ app/src/main/java/com/nhom4/lilpawhome_application/MainActivity.java
@@ -2,16 +2,20 @@
 package com.nhom4.lilpawhome_application;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
 
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
 import com.nhom4.adapters.Danhmuc1Adapter;
+import com.nhom4.adapters.HorSanPhamAdapter;
 import com.nhom4.lilpawhome_application.databinding.ActivityMainBinding;
 import com.nhom4.models.DanhMuc1;
 import com.nhom4.models.SanPham;
@@ -23,6 +27,13 @@ public class MainActivity extends AppCompatActivity {
     Danhmuc1Adapter adapter;
     ArrayList<DanhMuc1> danhmuc;
     ArrayList<SanPham> sanPhams;
+    ArrayList<SanPham> sanPhamsBanchay;
+    HorSanPhamAdapter adapter2;
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager RecyclerViewLayoutManager;
+    LinearLayoutManager Horizontallayout;
+
+
 
     private ImageSlider imageSlider;
     @Override
@@ -33,21 +44,11 @@ public class MainActivity extends AppCompatActivity {
         // setContentView(binding.getRoot());
         View view = binding.getRoot();
         setContentView(view);
-
-        imageSlider = findViewById(R.id.image_slider);
-        ArrayList<SlideModel> slideModels = new ArrayList<>();
-        slideModels.add(new SlideModel(R.drawable.lilpawhomebn,ScaleTypes.FIT));
-        slideModels.add(new SlideModel(R.drawable.khuyenmai20,ScaleTypes.FIT));
-        slideModels.add(new SlideModel(R.drawable.trangchushopchocho,ScaleTypes.FIT));
-        slideModels.add(new SlideModel(R.drawable.trangchushopchomeo,ScaleTypes.FIT));
-        slideModels.add(new SlideModel(R.drawable.trangchutintuc,ScaleTypes.FIT));
-        slideModels.add(new SlideModel(R.drawable.trangchuuudai,ScaleTypes.FIT));
-
-         imageSlider.setImageList(slideModels,ScaleTypes.FIT);
-        
+        loadBanner();
 
         loadData();
         addEvent();
+
     }
 
     private void addEvent() {
@@ -63,6 +64,27 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void loadData() {
+        loadDanhmuc();
+        loadSPgiamgia();
+        loadSPbanchay();
+
+
+
+    }
+    private void loadBanner(){
+        imageSlider = findViewById(R.id.image_slider);
+        ArrayList<SlideModel> slideModels = new ArrayList<>();
+        slideModels.add(new SlideModel(R.drawable.lilpawhomebn,ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.khuyenmai20,ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.trangchushopchocho,ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.trangchushopchomeo,ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.trangchutintuc,ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.trangchuuudai,ScaleTypes.FIT));
+
+        imageSlider.setImageList(slideModels,ScaleTypes.FIT);
+
+    }
+    private void loadDanhmuc(){
         danhmuc = new ArrayList<>();
         danhmuc.add(new DanhMuc1(R.drawable.icon_shopchocho_home,"Shop cho Chó"));
         danhmuc.add(new DanhMuc1(R.drawable.icon_shopchomeo_home,"Shop cho Mèo"));
@@ -74,6 +96,43 @@ public class MainActivity extends AppCompatActivity {
         adapter = new Danhmuc1Adapter(MainActivity.this,R.layout.danhmuc1_list,danhmuc);
         binding.gvDanhmuchome.setAdapter(adapter);
 
+    }
+    private void loadSPgiamgia(){
+        recyclerView = (RecyclerView) findViewById(R.id.rcv_spgiamgia);
+        RecyclerViewLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(RecyclerViewLayoutManager);
+        //add sp
+        sanPhams = new ArrayList<>();
+        sanPhams.add(new SanPham(R.drawable.hinhsanpham, "Thức ăn cho mèo Felipro 500g - Giảm sỏi mật - Vị hải sản", 32000, 36000, "Hãng: Felipiro", "Mèo", "Thức ăn"));
+        sanPhams.add(new SanPham(R.drawable.hinhsanpham, "Thức ăn cho mèo Felipro 500g - Giảm sỏi mật - Vị hải sản", 32000, 36000, "Hãng: Felipiro", "Mèo", "Thức ăn"));
+        sanPhams.add(new SanPham(R.drawable.hinhsanpham, "Thức ăn cho mèo Felipro 500g - Giảm sỏi mật - Vị hải sản", 32000, 36000, "Hãng: Felipiro", "Mèo", "Thức ăn"));
+        sanPhams.add(new SanPham(R.drawable.hinhsanpham, "Thức ăn cho mèo Felipro 500g - Giảm sỏi mật - Vị hải sản", 32000, 36000, "Hãng: Felipiro", "Mèo", "Thức ăn"));
+        sanPhams.add(new SanPham(R.drawable.hinhsanpham, "Thức ăn cho mèo Felipro 500g - Giảm sỏi mật - Vị hải sản", 32000, 36000, "Hãng: Felipiro", "Mèo", "Thức ăn"));
+
+        // truyen du lieu
+        adapter2= new HorSanPhamAdapter(sanPhams);
+        Horizontallayout=new LinearLayoutManager(MainActivity.this,LinearLayoutManager.HORIZONTAL,false);
+        recyclerView.setLayoutManager(Horizontallayout);
+        recyclerView.setAdapter(adapter2);
+
+    }
+    private void loadSPbanchay(){
+        recyclerView = (RecyclerView) findViewById(R.id.rcv_spBanchay);
+        RecyclerViewLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(RecyclerViewLayoutManager);
+        //add sp
+        sanPhamsBanchay = new ArrayList<>();
+        sanPhamsBanchay.add(new SanPham(R.drawable.hinhsanpham, "Thức ăn cho mèo Felipro 500g - Giảm sỏi mật - Vị hải sản", 32000, 36000, "Hãng: Felipiro", "Mèo", "Thức ăn"));
+        sanPhamsBanchay.add(new SanPham(R.drawable.hinhsanpham, "Thức ăn cho mèo Felipro 500g - Giảm sỏi mật - Vị hải sản", 32000, 36000, "Hãng: Felipiro", "Mèo", "Thức ăn"));
+        sanPhamsBanchay.add(new SanPham(R.drawable.hinhsanpham, "Thức ăn cho mèo Felipro 500g - Giảm sỏi mật - Vị hải sản", 32000, 36000, "Hãng: Felipiro", "Mèo", "Thức ăn"));
+        sanPhamsBanchay.add(new SanPham(R.drawable.hinhsanpham, "Thức ăn cho mèo Felipro 500g - Giảm sỏi mật - Vị hải sản", 32000, 36000, "Hãng: Felipiro", "Mèo", "Thức ăn"));
+        sanPhamsBanchay.add(new SanPham(R.drawable.hinhsanpham, "Thức ăn cho mèo Felipro 500g - Giảm sỏi mật - Vị hải sản", 32000, 36000, "Hãng: Felipiro", "Mèo", "Thức ăn"));
+
+        // truyen du lieu
+        adapter2= new HorSanPhamAdapter(sanPhamsBanchay);
+        Horizontallayout=new LinearLayoutManager(MainActivity.this,LinearLayoutManager.HORIZONTAL,false);
+        recyclerView.setLayoutManager(Horizontallayout);
+        recyclerView.setAdapter(adapter2);
 
     }
 
