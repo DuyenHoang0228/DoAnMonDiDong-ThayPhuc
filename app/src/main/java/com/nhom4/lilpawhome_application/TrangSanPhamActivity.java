322fea4fcdbd32c44477f03e061096d2fde86d61
nguyenngoctham02da <thamnn20411@st.uel.edu.vn>
2022-11-25 13:55:26 +0700

    Border chỗ discount, nút xem thêm và thu gọn nội dung trang sản phẩm, thanh navigation bar (chưa test)

--- app/src/main/java/com/nhom4/lilpawhome_application/TrangSanPhamActivity.java
+++ app/src/main/java/com/nhom4/lilpawhome_application/TrangSanPhamActivity.java
@@ -3,12 +3,65 @@ package com.nhom4.lilpawhome_application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.nhom4.adapters.SanphamAdapter;
+import com.nhom4.lilpawhome_application.databinding.ActivityTrangSanPhamBinding;
+import com.nhom4.models.SanPham;
+
+import java.util.ArrayList;
 
 public class TrangSanPhamActivity extends AppCompatActivity {
-
+    ActivityTrangSanPhamBinding binding;
+    SanphamAdapter adapter;
+    ArrayList<SanPham> sanPhamArrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_trang_san_pham);
+//        setContentView(R.layout.activity_trang_san_pham);
+        binding=ActivityTrangSanPhamBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.txtThongtinsanpham.setMaxLines(3);
+        loadData();
+        addEvents();
+    }
+
+    private void addEvents() {
+        binding.btnXemthem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(binding.btnXemthem.getText().toString().equalsIgnoreCase("Ẩn bớt"))
+                {
+                    binding.txtThongtinsanpham.setMaxLines(3);//your TextView
+                    binding.btnXemthem.setText("Xem thêm");
+                }else
+                {
+                    binding.txtThongtinsanpham.setMaxLines(Integer.MAX_VALUE);//your TextView
+                    binding.btnXemthem.setText("Ẩn bớt");
+                }
+            }
+        });
+    }
+
+    private void loadData() {
+        sanPhamArrayList=new ArrayList<>();
+        sanPhamArrayList.add(new SanPham(R.drawable.sphatcho,"Hạt cho chó",120000,200000,
+                "Thương hiệu 1","thucanchocho","hatchocho"));
+        sanPhamArrayList.add(new SanPham(R.drawable.sppatecho,"Pate cho chó",350000,400000,
+                "Thương hiệu 1","thucanchocho","patechocho"));
+        sanPhamArrayList.add(new SanPham(R.drawable.spsuacho,"Sữa tắm chó",250000,300000,
+                "Thương hiệu 2","thucanchocho","suacho"));
+        sanPhamArrayList.add(new SanPham(R.drawable.spsuatamcho,"Sữa tắm chó",120000,320000,
+                "Thương hiệu 2","dodungcho","suatamcho"));
+        sanPhamArrayList.add(new SanPham(R.drawable.spxuongcho,"Xương chó đồ chơi",20000,50000,
+                "Thương hiệu 3","dochoicho","xuongcho"));
+        sanPhamArrayList.add(new SanPham(R.drawable.spdinhduongcho,"Sữa dinh dưỡng cho chó",360000,500000,
+                "Thương hiệu 3","thucanchocho","dinhduongchocho"));
+        sanPhamArrayList.add(new SanPham(R.drawable.sptaimatmiengcho,"Cây chà răng chó",25000,40000,
+                "Thương hiệu 4","dodungcho","taimatcho"));
+
+        adapter=new SanphamAdapter(TrangSanPhamActivity.this,R.layout.list_sanpham_id,sanPhamArrayList);
+        binding.gvSsanphamdexuat.setAdapter(adapter);
+
     }
 }
