7f4b62013083c0a60b7f13469de167ff8e263479
DuyenHoang0228 <hoangduyen02281@gmail.com>
2022-11-25 21:44:59 +0700

    cập nhật

---
+++ app/src/main/java/com/nhom4/lilpawhome_application/UserActivity.java
@@ -0,0 +1,16 @@
+package com.nhom4.lilpawhome_application;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+
+
+    }
+}
