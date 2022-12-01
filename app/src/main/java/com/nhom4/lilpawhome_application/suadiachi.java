5a3a8277e70c45a9c60a3dea1552c580eb900452
DuyenHoang0228 <hoangduyen0228@gmail.com>
2022-12-01 16:17:11 +0700

    cập nhật

--- app/src/main/java/com/nhom4/lilpawhome_application/suadiachi.java
+++ app/src/main/java/com/nhom4/lilpawhome_application/suadiachi.java
@@ -1,14 +1,19 @@
 package com.nhom4.lilpawhome_application;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class suadiachi extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setContentView(R.layout.activity_suadiachi);
     }
 }
