9bc163588c297619854f42db1debca7da96e0569
XuanLeThiHong <xuanlth20411@st.uel.edu.vn>
2022-12-04 11:25:00 +0700

    têm spinner tknh, dia chi, dialog gg

--- app/src/main/java/com/nhom4/lilpawhome_application/dangky.java
+++ app/src/main/java/com/nhom4/lilpawhome_application/dangky.java
@@ -63,7 +63,7 @@ public class dangky extends AppCompatActivity {
                                 if (!matkhau.equals(nhaplaimatkhau)) {
                                     Toast.makeText(dangky.this, "Mật khẩu không trùng khớp", Toast.LENGTH_SHORT).show();
                                 }else {
-                                    OTPVetification_Dialog_Dangky otpVetification_dialog_dangky = new OTPVetification_Dialog_Dangky(dangky.this, binding.edtNhapemailhoacsdt.getText().toString());
+                                    OTPVetification_Dialog_Dangky otpVetification_dialog_dangky = new OTPVetification_Dialog_Dangky(dangky.this, binding.edtNhapsdt.getText().toString());
                                     otpVetification_dialog_dangky.setCancelable(false);
                                     otpVetification_dialog_dangky.show();
                                 }
@@ -83,7 +83,7 @@ public class dangky extends AppCompatActivity {
                                         Toast.makeText(dangky.this, "Bạn cần đồng ý với các điều khoản", Toast.LENGTH_SHORT).show();
                                     } else {
                                         //Hiện dialog nhập otp
-                                        OTPVetification_Dialog_Dangky otpVetification_dialog_dangky = new OTPVetification_Dialog_Dangky(dangky.this, binding.edtNhapemailhoacsdt.getText().toString());
+                                        OTPVetification_Dialog_Dangky otpVetification_dialog_dangky = new OTPVetification_Dialog_Dangky(dangky.this, binding.edtNhapsdt.getText().toString());
                                         otpVetification_dialog_dangky.setCancelable(false);
                                         otpVetification_dialog_dangky.show();
 //                                    Toast.makeText(dangky.this, "Tạo tài khoản thành công", Toast.LENGTH_SHORT).show();
